957f06aa5ba40c4bdb8b54678b4a09c8906daee5
devisree <devisreeprasadmudooru@gmail.com>
2025-11-05 14:05:11 +0530

    Use Railway PORT env for deployment

--- InfoHub-Challenge/server/server.js
+++ InfoHub-Challenge/server/server.js
@@ -67,6 +67,13 @@ app.get('/', (req, res) => {
   res.send('Express server is running!');
 });
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
