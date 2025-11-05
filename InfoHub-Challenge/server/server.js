c29a4207504f26261c3dcf85c3a4d646af06fd46
devisree <devisreeprasadmudooru@gmail.com>
2025-11-05 14:14:14 +0530

    Fix backend to use Railway PORT env

--- InfoHub-Challenge/server/server.js
+++ InfoHub-Challenge/server/server.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // 1. QUOTE GENERATOR ENDPOINT
 const quotes = [
@@ -72,6 +76,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 app.listen(PORT, () => {
