c98f8ca2ef8cde6830c6a73d16e9e4d2449d6bb7
devisree <devisreeprasadmudooru@gmail.com>
2025-11-05 14:25:49 +0530

    Fix wildcard route for Express3/path-to-regexp

--- InfoHub-Challenge/server/server.js
+++ InfoHub-Challenge/server/server.js
@@ -73,9 +73,12 @@ app.get('/', (req, res) => {
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+
+
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 
