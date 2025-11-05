82fdf4e4446c58a380569f84d9fc15f2df35e6ab
devisree <devisreeprasadmudooru@gmail.com>
2025-11-05 17:59:45 +0530

    changes in  app

--- InfoHub-Challenge/client/src/components/WeatherModule.jsx
+++ InfoHub-Challenge/client/src/components/WeatherModule.jsx
@@ -8,7 +8,7 @@ export default function WeatherModule() {
 
   useEffect(() => {
     setIsLoading(true);
-     axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/weather`)
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/weather`)
 
       .then(res => {
         setData(res.data);
