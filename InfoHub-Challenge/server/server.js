575c9b8d63d35a2a68a27e53b7d889c99a5bfe8d
devisree <devisreeprasadmudooru@gmail.com>
2025-11-05 14:33:59 +0530

    Fix wildcard catch-all route for production

--- InfoHub-Challenge/server/server.js
+++ InfoHub-Challenge/server/server.js
@@ -75,9 +75,10 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 
